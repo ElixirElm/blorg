9f339b9f8a9b687d9701326653e71450be3cbcb0
Umur Ozkul <umur.ozkul@gmail.com>
2017-05-27 03:31:41 +0200

    Phoenix 1.3 packages upgrade

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Blorg.Mixfile do
   def project do
     [app: :blorg,
      version: "0.0.1",
-     elixir: "~> 1.2",
+     elixir: "~> 1.4",
      elixirc_paths: elixirc_paths(Mix.env),
      compilers: [:phoenix, :gettext] ++ Mix.compilers,
      build_embedded: Mix.env == :prod,
@@ -30,7 +30,7 @@ defmodule Blorg.Mixfile do
   #
   # Type `mix help deps` for examples and options.
   defp deps do
-    [{:phoenix, "~> 1.2.1"},
+    [{:phoenix, "~> 1.3.0-rc"},
      {:phoenix_pubsub, "~> 1.0"},
      {:phoenix_ecto, "~> 3.0"},
      {:postgrex, ">= 0.0.0"},
